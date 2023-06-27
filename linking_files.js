0e6101fbb219d3a798b3302f5d98c9600612c546
Nathaniel-Peterson <135255679+Nathaniel-Peterson@users.noreply.github.com>
2023-06-27 18:40:53 -0500

    Initial commit
    Practicing linking files with JavaScript.

---
+++ linking_files.js
@@ -0,0 +1,4 @@
+//console.log("Nathaniel Peterson")
+/*This JavaScript file links to an html file to seperate the code 
+for better organization. The code above was commented out for educational
+purposes*/
