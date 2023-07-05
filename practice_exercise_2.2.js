4b501897d3e4eb5fff34c2313d14a5fa47710a28
Nathaniel-Peterson <135255679+Nathaniel-Peterson@users.noreply.github.com>
2023-07-05 18:42:27 -0500

    Practice Exercise 2.2
    Generated a message with the usage of addition.

---
+++ practice_exercise_2.2.js
@@ -0,0 +1,6 @@
+const myName = "Nathaniel";
+const myAge = "23";
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge + " years old and I can code JavaScript: " + coder + ".";
+
+console.log(message);
